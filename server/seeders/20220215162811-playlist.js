c46555da5b77a7d9c26b69056fcc4f0da14e1364
Kangbumkyu <g.kangbumkyu@gmail.com>
2022-02-18 11:16:16 +0900

    update: playlist & tag seeders

--- server/seeders/20220215162811-playlist.js
+++ server/seeders/20220215162811-playlist.js
@@ -1,16 +1,46 @@
 "use strict";
-
+const playlists = [
+  "조용한 노래",
+  "재즈 노래",
+  "모던한 노래",
+  "차분한 노래",
+  "뉴에이지 노래",
+  "신나는 노래",
+  "시끌벅적한 노래",
+  "일렉트로닉 노래",
+];
+const description = "듣고만 있어도 행복한 노래들과 더더욱 행복하세요 :)";
+const coverUrls = [
+  `${
+    process.env.NODE_ENV === "development"
+      ? process.env.DEV_DOMAIN + process.env.PORT + "/static"
+      : process.env.S3_DOMAIN
+  }/calm.jpg`,
+  `${
+    process.env.NODE_ENV === "development"
+      ? process.env.DEV_DOMAIN + process.env.PORT + "/static"
+      : process.env.S3_DOMAIN
+  }/party.jpg`,
+  `${
+    process.env.NODE_ENV === "development"
+      ? process.env.DEV_DOMAIN + process.env.PORT + "/static"
+      : process.env.S3_DOMAIN
+  }/quiet.jpg`,
+];
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert("Playlists", [
-      {
-        coverUrl: "ddd/ddd.com",
-        title: "happy",
-        description: "해피해",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    return queryInterface.bulkInsert(
+      "Playlists",
+      playlists.map((playlist) => {
+        return {
+          coverUrl: coverUrls[Math.floor(Math.random() * coverUrls.length)],
+          title: playlist,
+          description,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
   },
 
   async down(queryInterface, Sequelize) {
--- server/seeders/20220217025043-Playlist_Tag.js
+++ server/seeders/20220217025043-Playlist_Tag.js
@@ -16,3 +16,25 @@ module.exports = {
     return queryInterface.bulkDelete("Playlist_Tags", null, {});
   },
 };
+
+// "use strict";
+
+// let playlistTags = [];
+
+// for (let i = 1; i < 9; i++) {
+//   playlistTags.push({
+//     playlistId: i,
+//     tagId: Math.floor(Math.random() * 8) + 1,
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//   });
+// }
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     return queryInterface.bulkInsert("Playlist_Tags", playlistTags);
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     return queryInterface.bulkDelete("Playlist_Tags", null, {});
+//   },
+// };
