0cd1f98ec8cdba8e22edc2ef1715743e5e7007bb
Kangbumkyu <g.kangbumkyu@gmail.com>
2022-02-22 15:41:08 +0900

    feat: set isLoggedIn state

--- client/src/atom.js
+++ client/src/atom.js
@@ -4,3 +4,8 @@ export const isGlowingAtom = atom({
   key: "isGlowing",
   default: true,
 });
+
+export const isLoggedInAtom = atom({
+  key: "isLoggedIn",
+  default: localStorage.getItem("accessToken") ? true : false,
+});
