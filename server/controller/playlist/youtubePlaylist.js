14d5352ad7177a9e48cd95015cccd6c0a42ef112
Kangyong0527 <pgy0527@naver.com>
2022-02-25 11:48:58 +0900

    UPDATE AND FIX youtubePlaylist.js

--- server/controller/playlist/youtubePlaylist.js
+++ server/controller/playlist/youtubePlaylist.js
@@ -1,40 +1,49 @@
-const { response } = require("express");
 const { Playlist, Song } = require("../../models");
+const { isAuthorized } = require("../modules/tokenFunction");
 
 module.exports = {
   createPlaylist: async (req, res) => {
     const playlist = req.body;
     const coverFileUrl = req.file.path;
-    const songsParser = JSON.parse(playlist.songs);
+    // const songsParser = JSON.parse(playlist.songs);
+    const testSong = req.body.songs;
+    const authorization = req.headers.authorization;
+    const userId = isAuthorized(authorization);
     if (!playlist) {
-      res.status(400).send({ response: "잘못된 요청입니다" });
+      return res.status(400).send({ response: "잘못된 요청입니다" });
     }
     try {
-      const createPlaylist = Playlist.create({
-        coverUrl: coverFileUrl,
+      const createPlaylist = await Playlist.create({
+        coverUrl: process.env.DEV_DOMAIN + process.env.PORT + coverFileUrl,
         title: playlist.title,
         description: playlist.description,
+        userId: userId.id,
       });
-      const createSong = songsParser.map(async (data) => {
-        Song.create({
-          songUrl: data.url,
-          songTitle: data.title,
+      console.log(createPlaylist.dataValues.id);
+      const createSong = testSong.map(async (data) => {
+        await Song.create({
+          songUrl: data.songUrl,
+          songTitle: data.songTitle,
+          playlistId: createPlaylist.dataValues.id,
         });
       });
-      res.status(200).send({
+      return res.status(200).send({
         data: { playlists: createPlaylist, songs: createSong },
         response: "ok",
       });
     } catch (error) {
-      console.log("error : ", error);
+      // console.log("error : ", error);
     }
   },
   updatePlaylist: async (req, res) => {
     const updateBody = req.body;
     const updateFile = req.file.path;
-    const updateSongsParser = JSON.parse(updateBody.songs);
-    if (!request) {
-      res.status(400).send({ response: "잘못된 요청입니다" });
+    // const updateSongsParser = JSON.parse(updateBody.songs);
+    const authorization = req.headers.authorization;
+    const userId = isAuthorized(authorization);
+    const testSong = req.body.songs;
+    if (!updateBody) {
+      return res.status(400).send({ response: "잘못된 요청입니다" });
     }
     try {
       const updatePlaylist = await Playlist.update(
@@ -43,42 +52,48 @@ module.exports = {
           coverUrl: updateFile,
           title: updateBody.title,
           description: updateBody.description,
+          userId: userId.id,
           createdAt: updateBody.createdAt,
           updatedAt: updateBody.updatedAt,
         },
         { where: { id: updateBody.id } }
       );
-      const updateSong = updateSongsParser.map(async (song) => {
+      const updateSong = testSong.map(async (song) => {
         await Song.update(
           {
             id: song.id,
             songUrl: song.url,
             songTitle: song.title,
+            playlistId: song.playlistId,
             createdAt: song.createdAt,
             updatedAt: song.updatedAt,
           },
           { where: { id: song.id } }
         );
-        res.status(200).send({
+        return res.status(200).send({
           data: { playlists: updatePlaylist, songs: updateSong },
           response: "ok",
         });
       });
     } catch (error) {
-      res.status(500).send({ response: "err" });
+      return res.status(500).send({ error: error });
     }
   },
   deletePlaylist: async (req, res) => {
+    console.log(req.body);
+    const authorization = req.headers.authorization;
+    const userId = isAuthorized(authorization);
     const playlistId = await Playlist.findOne({
-      where: { id: req.body.playliteId },
+      where: { id: req.body.id },
     });
-    if (!playlistId) {
-      res.status(400).send({ response: "잘못된 접근입니다." });
+
+    if (!playlistId || !userId) {
+      return res.status(400).send({ response: "잘못된 접근입니다." });
     }
     try {
       await Playlist.destroy({ where: { id: playlistId.dataValues.id } });
       await Song.destroy({ where: { playlistId: playlistId.dataValues.id } });
-      res.status(200).send({ response: "ok" });
+      return res.status(200).send({ response: "ok" });
     } catch (error) {
       res.status(500).send("error : ", error);
     }
