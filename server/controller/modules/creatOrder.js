0a92646c484351180aa33d6ac5c38b5f3a51b7ad
Kangyong0527 <pgy0527@naver.com>
2022-03-03 15:56:34 +0900

    FIX: order db in userId

--- server/controller/modules/creatOrder.js
+++ server/controller/modules/creatOrder.js
@@ -1,13 +1,14 @@
 const { Order } = require("../../models");
 
 module.exports = {
-  creatOrder: async (tag, time) => {
-    if (tag === null || time === null) {
+  creatOrder: async (tag, time, id) => {
+    if (tag === null || time === null || id === null) {
       return console.log("err");
     }
     await Order.create({
       theme: tag,
       time: time,
+      userId: id,
     });
   },
 };
