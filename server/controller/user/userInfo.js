3d685678adcf444b9fa9a143e2df1f9efe217233
sang <tkddjs5@naver.com>
2022-03-03 16:11:27 +0900

    Fix userinfo

--- server/controller/user/userInfo.js
+++ server/controller/user/userInfo.js
@@ -14,13 +14,19 @@ module.exports = async (req, res) => {
           id: accessToken.id,
         },
       });
+      const userOrderData = await Order.findOne({
+        where: {
+          id: accessToken.id,
+        },
+      });
+      const weekVisitCount = {};
       return res.status(200).send({
         data: {
           username: userData.username,
-          useremail: userData.useremail,
+          useremail: userData.email,
           visitCount: userData.visitCount,
-          weekVisitCoint: "0",
-          totalHour: "13.5",
+          weekVisitCount: "0",
+          totalHour: userOrderData.time,
         },
         response: "ok",
       });
